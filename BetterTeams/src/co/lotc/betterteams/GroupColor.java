fe45cbf96c91fa4792e470a02f70db0fb0945c56
Kalen <501warhead@gmail.com>
2016-01-12 19:06:08 -0500

    The MAT the MAT

--- BetterTeams/src/co/lotc/betterteams/GroupColor.java
+++ BetterTeams/src/co/lotc/betterteams/GroupColor.java
@@ -7,16 +7,17 @@ public enum GroupColor
 {
     ADMIN("ADMIN", 0, new StringBuilder().append(ChatColor.DARK_RED).append(ChatColor.BOLD).toString(), "Admin"),
     TECHNICIAN("TECHNICIAN", 1, new StringBuilder().append(ChatColor.GOLD).append(ChatColor.BOLD).toString(), "Technician"), 
-    MODERATOR("MODERATOR", 2, new StringBuilder().append(ChatColor.DARK_BLUE).append(ChatColor.BOLD).toString(), "Moderator"), 
-    ET("ET", 3, new StringBuilder().append(ChatColor.DARK_GREEN).toString(), "ET"),
-    LORE("LORE", 4, new StringBuilder().append(ChatColor.GREEN).toString(), "Lore"),
-    VIP500("VIP500", 5, new StringBuilder().append(ChatColor.YELLOW).append(ChatColor.BOLD).toString(), "VIP500"), 
-    VIP300("VIP300", 6, new StringBuilder().append(ChatColor.DARK_PURPLE).toString(), "VIP300"), 
-    VIP200("VIP200", 7, new StringBuilder().append(ChatColor.LIGHT_PURPLE).toString(), "VIP200"), 
-    VIP100("VIP100", 8, new StringBuilder().append(ChatColor.AQUA).toString(), "VIP100"), 
-    VIP50("VIP50", 9, new StringBuilder().append(ChatColor.GOLD).toString(), "VIP50"), 
-    VIP25("VIP25", 10, new StringBuilder().append(ChatColor.YELLOW).toString(), "VIP25"), 
-    VIP10("VIP10", 11, new StringBuilder().append(ChatColor.DARK_GRAY).toString(), "VIP10");
+    MODERATOR("MODERATOR", 2, new StringBuilder().append(ChatColor.DARK_BLUE).append(ChatColor.BOLD).toString(), "Moderator"),
+    MAT("MAT", 3, new StringBuilder().append(ChatColor.DARK_AQUA).toString(), "MAT"),
+    ET("ET", 4, new StringBuilder().append(ChatColor.DARK_GREEN).toString(), "ET"),
+    LORE("LORE", 5, new StringBuilder().append(ChatColor.GREEN).toString(), "Lore"),
+    VIP500("VIP500", 6, new StringBuilder().append(ChatColor.YELLOW).append(ChatColor.BOLD).toString(), "VIP500"), 
+    VIP300("VIP300", 7, new StringBuilder().append(ChatColor.DARK_PURPLE).toString(), "VIP300"), 
+    VIP200("VIP200", 8, new StringBuilder().append(ChatColor.LIGHT_PURPLE).toString(), "VIP200"), 
+    VIP100("VIP100", 9, new StringBuilder().append(ChatColor.AQUA).toString(), "VIP100"), 
+    VIP50("VIP50", 10, new StringBuilder().append(ChatColor.GOLD).toString(), "VIP50"), 
+    VIP25("VIP25", 11, new StringBuilder().append(ChatColor.YELLOW).toString(), "VIP25"), 
+    VIP10("VIP10", 12, new StringBuilder().append(ChatColor.DARK_GRAY).toString(), "VIP10");
     
     private final String color;
     private final String group;
