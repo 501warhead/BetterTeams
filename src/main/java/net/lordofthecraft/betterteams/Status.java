99cefef983e35c5af851282309f09fe49a2d9fa5
Kowaman <nickinpie@gmail.com>
2018-04-14 18:31:20 -0400

    Recolouring statuses for clarity.

--- src/main/java/net/lordofthecraft/betterteams/Status.java
+++ src/main/java/net/lordofthecraft/betterteams/Status.java
@@ -4,20 +4,20 @@ import org.bukkit.ChatColor;
 
 public enum Status
 {
-    AFFECTIONATE(	ChatColor.DARK_RED 		+ "\u2764"), 
+    AFFECTIONATE(	ChatColor.LIGHT_PURPLE 	+ "\u2764"), 
     CRIPPLED(		ChatColor.BLUE 			+ "\u267F"), 
     PIRATING(		ChatColor.DARK_GRAY 	+ "\u2620"),
     FIGHTING(		ChatColor.RED 			+ "\u2694"), 
     MOUNTED(		ChatColor.BOLD 			+ "\u265e"), 
     MUSICAL(		ChatColor.GREEN 		+ "\u266b"), 
     CASTING(		ChatColor.GOLD 			+ "\u272e"), 
-    BLINDED(		ChatColor.LIGHT_PURPLE 	+ "\u29de"),
+    BLINDED(		ChatColor.GRAY 			+ "\u29de"),
     FLORAL(			ChatColor.DARK_PURPLE 	+ "\u2740"),
     FROZEN(			ChatColor.AQUA			+ "\u2744"),
     SLEEPING(		ChatColor.DARK_GREEN 	+ "\u262a"),
     CRUSADING(		ChatColor.YELLOW 		+ "\u2021"),
     FLAGGED(		ChatColor.DARK_AQUA		+ "\u2691"),
-    RECORDING(		ChatColor.GRAY			+ "\u25CF");
+    RECORDING(		ChatColor.RED			+ "\u25CF");
     //UNDEAD(       ChatColor.BLACK         + "\u2742");
     
     private final String icon;
